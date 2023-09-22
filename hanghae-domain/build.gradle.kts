7b0e817bdc1ecb35c1214c1d87fce687ef65321c
jaypark <pjhyun1990@gmail.com>
2023-09-22 21:49:20 +0900

    multi-module docker build setting

--- hanghae-domain/build.gradle.kts
+++ hanghae-domain/build.gradle.kts
@@ -4,3 +4,14 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
+docker {
+    name = project.name + ":" + version
+    setDockerfile(file("../Dockerfile"))
+    files(tasks.bootJar.get().outputs.files)
+    buildArgs(
+        mapOf(
+            "JAR_FILE" to tasks.bootJar.get().outputs.files.singleFile.name
+        )
+    )
+}
