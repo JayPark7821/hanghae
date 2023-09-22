1d6c5780f03cbe93c92d49ae985af0a40b981cdd
jaypark <pjhyun1990@gmail.com>
2023-09-22 21:47:00 +0900

    multi-module setting

---
+++ hanghae-domain/src/main/kotlin/kr/jay/hanghaedomain/HanghaeDomainApplication.kt
@@ -0,0 +1,11 @@
+package kr.jay.hanghaedomain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanghaeDomainApplication
+
+fun main(args: Array<String>) {
+    runApplication<HanghaeDomainApplication>(*args)
+}
---
+++ hanghae-domain/src/test/kotlin/kr/jay/hanghaedomain/HanghaeDomainApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.jay.hanghaedomain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HanghaeDomainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
